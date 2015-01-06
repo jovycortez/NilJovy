19b06b9361a6476808be0c2f8addf298921b1b56
Simmigon <simmigon.flagg@gmail.com>
2015-01-06 17:49:34 -0500

    My Changes too!

--- src/NilJovy.java
+++ src/NilJovy.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
 
 public class NilJovy {
-
+static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String name;
+		System.out.println("Hi, My name is Simmigon\nWhat is your name?" );
+		System.out.println("Hi , "+(name = input.next()) +"!");
+		
 	}
 
 }
